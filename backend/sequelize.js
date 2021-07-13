cdfa2e20b99fa68d0ec986592ebe9d9ac538992b
Tristan <citrynox9@gmail.com>
2021-07-13 15:55:48 +0200

    Commit #5 Backend
    Modification :
    
    Connection DB
    
    Création :

---
+++ backend/sequelize.js
@@ -0,0 +1,21 @@
+const Sequelize = require('sequelize');
+
+
+const sequelize = new Sequelize('groupomania', 'root', '', {
+    host: 'localhost',
+    dialect: 'mysql',
+    port: 3308,
+    pool: {
+        max: 10,
+        min: 0,
+        acquire: 30000,
+        idle: 10000
+    }
+});
+
+sequelize.sync({})
+    .then(() => {
+        console.log('Database & tables created!');
+    });
+
+module.exports = {};

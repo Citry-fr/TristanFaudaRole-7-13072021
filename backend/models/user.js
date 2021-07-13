9275697f8e3fc48c5513be324f121e568603a5e9
Tristan <citrynox9@gmail.com>
2021-07-13 16:22:09 +0200

    Commit #6 Backend
    Modification :
    
    Création user model
    
    Création :
    
    ./backend/models/user.js

---
+++ backend/models/user.js
@@ -0,0 +1,21 @@
+module.exports = (sequelize, type) => {
+    return sequelize.define('user', {
+        id: {
+            type: type.INTEGER,
+            primaryKey: true,
+            autoIncrement: true
+        },
+        email: {
+            type: type.STRING,
+            unique: true
+        },
+        password: type.STRING,
+        firstName: type.STRING,
+        lastName: type.STRING,
+        rank: {
+            max: 1,
+            min: 0,
+            type: type.INTEGER
+        }
+    })
+};

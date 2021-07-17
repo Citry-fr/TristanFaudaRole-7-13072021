4cbc223a0c2a67214288610a78748a4f500bab5b
Tristan <citrynox9@gmail.com>
2021-07-17 15:42:30 +0200

    Commit #21 Backend
    Modification :
    
    Création route modification et suppression de commentaire
    
    Création :

--- backend/controllers/comCtrl.js
+++ backend/controllers/comCtrl.js
@@ -7,3 +7,22 @@ exports.postComment = (req, res, next) => {
         gifId: req.params.gifId
     }).then(com => res.json(com));
 }
+
+exports.modifyComment = (req, res, next) => {
+    Comment.findOne({ where: { id: req.params.comId }})
+        .then(com => {
+            const comObject = req.body;
+            com.update({
+                ...comObject,
+                gifId: req.params.gifId
+            }).then(com => res.json(com));
+        }).catch(error => res.status(400).json({ error }));
+}
+exports.deleteComment = (req, res, next) => {
+    Comment.findOne({ where: { id: req.params.comId }})
+        .then(com => {
+            com.destroy()
+                .then(res.status(200).json({ message: "Commentaire Supprimé !"}))
+                //.catch(error => res.status(500).json({ error }));
+        });
+}
--- backend/routes/comRte.js
+++ backend/routes/comRte.js
@@ -5,5 +5,7 @@ const comCtrl = require('../controllers/comCtrl');
 const auth = require('../middlewares/auth');
 
 router.post('/:gifId', auth, comCtrl.postComment);
+router.put('/:gifId/:comId', auth, comCtrl.modifyComment);
+router.delete('/:comId', auth, comCtrl.deleteComment);
 
 module.exports = router;

730bdb76a52874ca692aa6d069ee07f14812d72a
Tristan <citrynox9@gmail.com>
2021-07-25 14:27:47 +0200

    Création composant SignupForm

---
+++ frontend/app/src/components/SignupForm.vue
@@ -0,0 +1,24 @@
+<template>
+    <div class="form">
+        <div class="form__email">
+            <label class="form__email__label" for="email">Email :</label>
+            <input class="form__email__input" type="email" name="email">
+        </div>
+        <div class="form__password">
+            <label class="form__password__label" for="password">Mot de passe :</label>
+            <input class="form__password__input" type="password" name="password">
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'SignupForm'
+}
+</script>
+
+<style lang="scss" scoped>
+    .form{
+        display: flex;
+    }
+</style>
--- frontend/app/src/views/Signup.vue
+++ frontend/app/src/views/Signup.vue
@@ -1,3 +1,18 @@
 <template>
-    <p>Page formulaire pour s'inscrire</p>
+    <div>
+        <h1>Formulaire d'inscription</h1>
+        <SignupForm />
+    </div>
 </template>
+
+<script>
+
+import SignupForm from '../components/SignupForm.vue'
+
+export default {
+    name: 'Signup',
+    components: {
+        SignupForm
+    }
+}
+</script>

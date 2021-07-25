7241612cea1de1bb2d50f97f78e7dd767fb8d259
Tristan <citrynox9@gmail.com>
2021-07-25 15:46:56 +0200

    Style pour le composant 'SignupForm'

--- frontend/app/src/App.vue
+++ frontend/app/src/App.vue
@@ -19,8 +19,11 @@
 </template>
 
 <style>
+
+@import url('https://fonts.googleapis.com/css2?family=Nunito&display=swap');
+
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
+  font-family: 'Nunito', sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
--- frontend/app/src/components/SignupForm.vue
+++ frontend/app/src/components/SignupForm.vue
@@ -2,11 +2,19 @@
     <div class="form">
         <div class="form__email">
             <label class="form__email__label" for="email">Email :</label>
-            <input class="form__email__input" type="email" name="email">
+            <input class="form__email__input" type="email" name="email" id="email">
         </div>
         <div class="form__password">
             <label class="form__password__label" for="password">Mot de passe :</label>
-            <input class="form__password__input" type="password" name="password">
+            <input class="form__password__input" type="password" name="password" id="password">
+        </div>
+        <div class="form__firstname">
+            <label class="form__firstname__label" for="firstname">Prénom :</label>
+            <input class="form__firstname__input" type="text" name="firstname" id="firstname">
+        </div>
+        <div class="form__lastname">
+            <label class="form__lastname__label" for="lastname">Nom :</label>
+            <input class="form__lastname__input" type="text" name="lastname" id="lastname">
         </div>
     </div>
 </template>
@@ -20,5 +28,35 @@ export default {
 <style lang="scss" scoped>
     .form{
         display: flex;
+        justify-content: center;
+        flex-direction: column;
+        gap: 20px;
+        &__email, &__password, &__firstname, &__lastname{
+            display: flex;
+            justify-content: center;
+            flex-direction: column;
+            align-items: center;
+        }
+              
+    }
+    label {
+        font-weight: bold;
+        font-size: 1.5em;
+        width: 400px;
+        display: flex;
+    }
+
+    input {
+        width: 400px;
+        height: 30px;
+        font-size: 1.2em;
+        border-radius: 10px;
+        border: solid 2px #2c3e50;
+        padding-left: 10px;
+        font-family: 'Nunito', sans-serif;
+        outline: none;
+        &:focus{
+            border-color: #FD2D01;
+        }
     }
 </style>

b8181891004881ae0110a152545c05c96ac6fd21
Tristan <citrynox9@gmail.com>
2021-07-29 14:25:45 +0200

    Installation plus configuration de multer

---
+++ backend/middlewares/multer-config.js
@@ -0,0 +1,18 @@
+const multer = require('multer');
+
+const MIMES_TYPES = {
+    'image/gif': 'gif'
+};
+
+const storage = multer.diskStorage({
+    destination: (req, file, callback) => {
+        callback(null, 'images');
+    },
+    filename: (req, file, callback) => {
+        const name = file.originalname.split(' ').join('_');
+        const extension = MIMES_TYPES[file.mimetype];
+        callback(null, name + Date.now() + '.' + extension);
+    }
+});
+
+module.exports = multer({storage: storage}).single('image');
